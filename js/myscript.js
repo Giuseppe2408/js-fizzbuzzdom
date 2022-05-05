ce62f3ed16d46ad070f0001df08edae6d3875da2
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-05 17:16:03 +0200

    credo di avercela fatta

--- js/myscript.js
+++ js/myscript.js
@@ -1,32 +1,27 @@
 // scrivere programma che stampi i numeri in console da 1 a 100
-
-// const ul = document.querySelector ("ul.list")
-// const li = document.createElement ("li")
-for ( let i = 1; i <= 100; i++) {
-    const multiplo3 = 3;
-    const multiplo5 = 5;
-    const multiplo3_5 = 15;
-    const ul = document.querySelector ("ul.list")
-    const li = document.createElement ("li")
-    console.log(i);
-    li.append(i);
-    ul.append(li);
-    if (i % multiplo3==0) {
-        li.append("fizz");
-        ul.append(li);
+let i = 1;
+const container = document.querySelector(".container");
+for (i = 1; i <= 100; i++) {
+    let square;
+    // i multipli di 3 colorarli con fizz, multipli 5 buzz multipli entambi fizzbuzz
+    if ( i % 3==0) {
+        square = `<div class="fizz justify-content-center align-items-center flex-wrap box mx-3 my-3">`;
     }
 
-    else if (i % multiplo5==0) {
-        li.append("buzz");
-        ul.append(li);    
+    else if (i % 5==0) {
+        square = `<div class="buzz justify-content-center align-items-center flex-wrap box mx-3 my-3">`; 
     }
 
-    else if (i % multiplo3_5==0) {
-        li.append("fizz" + "buzz");
-        ul.append(li); 
+    else if (i % 3==0 && i % 5==0) {
+        square = `<div class="fizzbuzz justify-content-center align-items-center flex-wrap box mx-3 my-3">`;
     }
-    
+
+    else {
+        square = `<div class="fizzbuzz justify-content-center align-items-center flex-wrap box mx-3 my-3">`;
+    }
+
+    container.innerHTML += square;
 }
-// i multipli di 3 colorarli con fizz, multipli 5 buzz multipli entambi fizzbuzz
+
 // appendi elementi nel dom
 // stile diverso per i multipli
