7c5e321e81dd20e35991756a95b910b34bf716d6
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-06 09:46:15 +0200

    correzioni

--- js/myscript.js
+++ js/myscript.js
@@ -7,17 +7,17 @@ for (i = 1; i <= 100; i++) {
     // i multipli di 3 colorarli con fizz, multipli 5 buzz multipli entambi fizzbuzz
     if (i % 3==0 && i % 5==0) {
         square = `<div class="fizzbuzz justify-content-center align-items-center flex-wrap box mx-3 my-3">`;
-        console.log("fizzbuzz")
+        console.log("fizzbuzz");
     }
     
     else if (i % 5==0) {
         square = `<div class="buzz justify-content-center align-items-center flex-wrap box mx-3 my-3">`; 
-        console.log("buzz")
+        console.log("buzz");
     }
 
     else if ( i % 3==0) {
         square = `<div class="fizz justify-content-center align-items-center flex-wrap box mx-3 my-3">`;
-        console.log("fizz")
+        console.log("fizz");
     }
 
     else {
